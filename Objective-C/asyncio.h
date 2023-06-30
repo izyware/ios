1ee29872b4bda4575f9abddafe7520eb441e4551
Evgheni.Antropov <evgheniantropov@izyware.com>
2023-06-30 18:33:14 +0000

    6900007: asyncio - implement IZYrun macro to simplfy chain action process in ObjectiveC

--- Objective-C/asyncio.h
+++ Objective-C/asyncio.h
@@ -1,5 +1,8 @@
 #import "lib/json.h"
 #import "lib/monitoring.h"
+
+#define IZYrun(_action, _queryObject) [izyAsyncIO run:_action queryObject:_queryObject];
+
 @interface izyAsyncIO : NSObject
 // class method (not interface method)
 + (NSDictionary* )run:(NSString*) action queryObject:(id)queryObject;
